886e58f8becacb828ee124714097e5d86c33ee43
zhuowenli <zhuowenligg@gmail.com>
2020-09-02 11:25:33 +0800

    :tada: Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
 # egg-typeorm
-TypeORM for Egg.js
+
+[TypeORM](https://typeorm.io/#/) for [Egg.js](https://eggjs.org/).
+
+[![NPM version][npm-image]][npm-url]
+[![npm download][download-image]][download-url]
+
+[npm-image]: https://img.shields.io/npm/v/@zhuowenli/egg-typeorm.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/@zhuowenli/egg-typeorm
+[download-image]: https://img.shields.io/npm/dm/@zhuowenli/egg-typeorm.svg?style=flat-square
+[download-url]: https://npmjs.org/package/@zhuowenli/egg-typeorm
+
+## Install
+
+```bash
+yarn add @zhuowenli/egg-typeorm mysql
+```
+
+## Usage
+
+### Plugin
+
+```ts
+// {app_root}/config/plugin.ts
+const plugin: EggPlugin = {
+    typeorm: {
+        enable: true,
+        package: '@zhuowenli/egg-typeorm',
+    },
+};
+```
+
+### Configuration
+
+```ts
+// {app_root}/config/config.default.ts
+config.typeorm = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'test',
+    synchronize: true,
+    logging: false,
+    entities: ['app/entity/**/*.ts'],
+    migrations: ['app/migration/**/*.ts'],
+    subscribers: ['app/subscriber/**/*.ts'],
+    cli: {
+        entitiesDir: 'app/entity',
+        migrationsDir: 'app/migration',
+        subscribersDir: 'app/subscriber',
+    },
+};
+```
+
+### Create entity files
+
+```bash
+├── controller
+│   └── home.ts
+├── entity
+    ├── Post.ts
+    └── User.ts
+```
+
+### Entity file
+
+```ts
+// app/entity/User.ts
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+}
+```
+
+### Use with FindOptions
+
+```ts
+// in controller
+export default class UserController extends Controller {
+    public async index() {
+        const { ctx } = this;
+        ctx.body = await ctx.repo.User.find();
+    }
+}
+```
+
+### Use with QueryBuilder
+
+```ts
+// in controller
+export default class UserController extends Controller {
+    public async index() {
+        const { ctx } = this;
+        const firstUser = await ctx.repo.User.createQueryBuilder('user')
+            .where('user.id = :id', { id: 1 })
+            .getOne();
+        ctx.body = firstUser;
+    }
+}
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ app.ts
@@ -0,0 +1,219 @@
+/*
+ * Author: 卓文理
+ * Email: zhuowenligg@gmail.com
+ * Date: 2020-09-02 11:08:45
+ */
+
+import { join, sep } from 'path';
+import { find } from 'fs-jetpack';
+import { Application } from 'egg';
+import {
+    createConnection,
+    getRepository,
+    Connection,
+    useContainer,
+} from 'typeorm';
+import { Container } from 'typedi';
+
+import { watch } from 'chokidar';
+import * as fs from 'fs-extra';
+import * as prettier from 'prettier';
+
+const hasTsLoader = typeof require.extensions['.ts'] === 'function';
+
+export function formatCode(text: string) {
+    return prettier.format(text, {
+        semi: false,
+        tabWidth: 2,
+        singleQuote: true,
+        parser: 'typescript',
+        trailingComma: 'all',
+    });
+}
+
+function handleConfig(config: any) {
+    if (hasTsLoader) {
+        return config;
+    }
+    const keys = ['entities', 'migrations', 'subscribers'];
+    for (const key of keys) {
+        if (config[key]) {
+            const newValue = config[key].map((item: string) =>
+                item.replace(/\.ts$/, '.js'),
+            );
+            config[key] = newValue;
+        }
+    }
+    return config;
+}
+
+async function connectDB(app: Application) {
+    const config = handleConfig(app.config.typeorm);
+    useContainer(Container);
+
+    const connection = await createConnection(config);
+    app.context.connection = connection;
+}
+
+function capitalizeFirstLetter(str: string) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function getModelName(file: string) {
+    const filename = file.split(sep).pop() || '';
+    const name = capitalizeFirstLetter(filename.replace(/\.ts$|\.js$/g, ''));
+    return name;
+}
+
+function writeTyping(path: string, text: string) {
+    fs.writeFileSync(path, formatCode(text), { encoding: 'utf8' });
+}
+
+function getTypingText(
+    importText: string,
+    repoText: string,
+    entityText: string,
+) {
+    const tpl = `
+import 'egg';
+import { Repository, Connection } from 'typeorm';
+${importText}
+declare module 'egg' {
+    interface Context {
+        connection: Connection
+        entity: {
+            ${entityText}
+        }
+        repo: {
+            ${repoText}
+        }
+    }
+}
+`;
+    return tpl;
+}
+
+function formatPaths(files: string[]) {
+    return files.map(file => {
+        const name = getModelName(file);
+        file = file.split(sep).join('/');
+        const importPath = `../${file}`.replace(/\.ts$|\.js$/g, '');
+        return {
+            name,
+            importPath,
+        };
+    });
+}
+
+function watchEntity(app: Application) {
+    const { baseDir } = app;
+    const entityDir = join(baseDir, 'app', 'entity');
+    const typingsDir = join(baseDir, 'typings');
+
+    if (!fs.existsSync(entityDir)) return;
+
+    fs.ensureDirSync(typingsDir);
+
+    watch(entityDir).on('all', (eventType: string) => {
+        if (['add', 'change'].includes(eventType)) {
+            createTyingFile(app);
+        }
+
+        if (['unlink'].includes(eventType)) {
+            createTyingFile(app);
+        }
+    });
+}
+
+function createTyingFile(app: Application) {
+    const { baseDir } = app;
+    const entityDir = join(baseDir, 'app', 'entity');
+    const files = find(entityDir, { matching: '*.ts' });
+    const typingPath = join(baseDir, 'typings', 'typeorm.d.ts');
+    const pathArr = formatPaths(files);
+    const importText = pathArr
+        .map(i => `import ${i.name} from '${i.importPath}'`)
+        .join('\n');
+    const repoText = pathArr
+        .map(i => `${i.name}: Repository<${i.name}>`)
+        .join('\n');
+
+    // TODO
+    const entityText = pathArr.map(i => `${i.name}: any`).join('\n');
+    const text = getTypingText(importText, repoText, entityText);
+    writeTyping(typingPath, text);
+}
+
+function getEntityFromPath(app: Application, entityPath: string) {
+    const connection: Connection = app.context.connection;
+    const fileModule = require(entityPath);
+    const entities = Object.keys(fileModule).reduce(
+        (result, cur) => {
+            try {
+                // TODO: 太 hack
+                connection.getMetadata(fileModule[cur]);
+                if (!result.includes(fileModule[cur])) {
+                    return [...result, fileModule[cur]];
+                }
+                return result;
+            } catch {
+                //
+            }
+
+            return result;
+        },
+    [] as any[],
+    );
+
+    if (!entities.length) {
+        throw new Error(`${entityPath} 格式不正确，不存在 @entity`);
+    }
+
+    return entities[0];
+}
+
+async function loadEntityAndModel(app: Application) {
+    const { baseDir } = app;
+    const entityDir = join(baseDir, 'app', 'entity');
+
+    if (!fs.existsSync(entityDir)) return;
+
+    const matching = hasTsLoader ? '*.ts' : '*.js';
+
+    const files = find(entityDir, { matching });
+    app.context.repo = {};
+    app.context.entity = {};
+
+    try {
+        for (const file of files) {
+            const entityPath = join(baseDir, file);
+            const entity: any = getEntityFromPath(app, entityPath);
+            const name = getModelName(file);
+            app.context.repo[name] = getRepository(entity);
+            app.context.entity[name] = entity;
+        }
+    } catch (e) {
+        app.logger.error(e);
+    }
+}
+
+export default async (app: Application) => {
+    const config = app.config.typeorm;
+    if (!config) {
+        throw new Error('please config typeorm in config file');
+    }
+
+    app.beforeStart(async () => {
+        try {
+            await connectDB(app);
+            // if (app.config.env === 'local') {
+            watchEntity(app);
+            // }
+            await loadEntityAndModel(app);
+            app.logger.info('[typeorm]', '数据链接成功');
+        } catch (error) {
+            app.logger.error('[typeorm]', '数据库链接失败');
+            app.logger.error(error);
+        }
+    });
+};
